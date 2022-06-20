637b42f7f344c69d22cb7426c2f7f01894403e84
anela <anela89@gmail.com>
2022-06-20 14:01:28 -0700

    added API routes to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, url_for, render_template, redirect, flash, jsonify
+from numpy import size
+from flask import Flask, url_for, render_template, redirect, flash, jsonify, request
 
 # from flask_debugtoolbar import DebugToolbarExtension
 
@@ -25,3 +26,56 @@ connect_db(app)
 # toolbar = DebugToolbarExtension(app)
 
 
+@app.get("/api/cupcakes")
+def cupcakes_get_data():
+    """
+    Get list of all cupcakess and their data. Returns with JSON:
+        {cupcakes: [{id, flavor, size, rating, image}, ...]}
+    """
+
+    cupcakes = Cupcake.query.all()
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+@app.get("/api/cupcakes/<int:cupcake-id>")
+def cupcake_get_data(cupcake_id):
+    """
+    Get data about a single cupcake. Returns with JSON:
+        {cupcake: {id, flavor, size, rating, image}}
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialize = cupcake.serialize()
+
+    return jsonify(cupcake=serialize)
+
+
+@app.post("/api/cupakes")
+def cupcake_create():
+    """
+    Creates a cupcake with id, flavor, size, rating, and image.
+    Returns with JSON:
+        {cupcake: {id, flavor, size, rating, image}}
+    """
+
+    name = request.json["name"]
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(
+        name=name,
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
