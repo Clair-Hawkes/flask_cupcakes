8e7be93f196250a97f4de137514fde80b0b4c57c
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-20 12:28:38 -0700

    Create models.py and Seed database

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, url_for, render_template, redirect, flash, jsonify
+
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+# from forms import AddPetForm, EditPetForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+# db.create_all()
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# toolbar = DebugToolbarExtension(app)
+
+
--- models.py
+++ models.py
@@ -1,1 +1,47 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect the models.py file to our Flask Application"""
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(20),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image = db.Column(
+        db.Text,
+        default='https://tinyurl.com/demo-cupcake',
+        nullable=False
+    )
+
+
+
+
+
